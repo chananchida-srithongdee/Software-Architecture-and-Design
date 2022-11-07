b9fc2e27fdab41f4b4c78d8a1d9ba3432a66dc84
chananchida-srithongdee <63010187@kmitl.ac.th>
2022-11-07 22:25:05 +0700

    Init folders: behavioral and structural

---
+++ structural/HealthcareWorkerTeam.java
@@ -0,0 +1,11 @@
+public class HealthcareWorkerTeam {
+    // Implement this file, and don't modify any other files.
+
+    /*
+     * Your tasks for this assignment require you to implement the following file:
+    HealthcareWorkerTeam.java - a container class for the Composite Pattern.
+
+    Your task involves Composite Pattern. Please look at HealthcareWorker.svg and create HealthcareWorkerTeam.java accordingly. 
+    You may look at Main.java to see the expected usages.
+     */
+}
